fa675e5f0175447693330d7ab52d40125a674d0e
yellowbag43 <yellowbag43@gmail.com>
2022-11-15 22:14:58 +0530

    first build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUserURL: "http://localhost:4005/mk-api/v1/user/",
+  apiPassURL: "http://localhost:4005/mk-api/v1/password/",
+  apiEmployeeURL: "http://localhost:4005/mk-api/v1/employee/",
+  apiJobURL: "http://localhost:4005/mk-api/v1/job/",
+  apiAttendanceURL: "http://localhost:4005/mk-api/v1/attendance/"
+
 };
