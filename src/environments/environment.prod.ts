e538ba0e6b0eafd482af7b2def164013d290eb2e
yellowbag43 <yellowbag43@gmail.com>
2022-11-16 12:25:31 +0530

    aws

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUserURL: "http://yellowbag.online/mk-api/v1/user/",
-  apiPassURL: "http://yellowbag.online/mk-api/v1/password/",
-  apiEmployeeURL: "http://yellowbag.online/mk-api/v1/employee/",
-  apiJobURL: "http://yellowbag.online/mk-api/v1/job/",
-  apiAttendanceURL: "http://yellowbag.online/mk-api/v1/attendance/"
+  apiUserURL: "http://13.115.154.253:80/mk-api/v1/user/",
+  apiPassURL: "http://13.115.154.253:80/mk-api/v1/password/",
+  apiEmployeeURL: "http://13.115.154.253:80/mk-api/v1/employee/",
+  apiJobURL: "http://13.115.154.253:80/mk-api/v1/job/",
+  apiAttendanceURL: "http://13.115.154.253:80/mk-api/v1/attendance/"
 
 };
